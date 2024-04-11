f9e835d2f50b51dfb5d975a96cd31d2e0f719a69
jeemh <jimyeongheon@gmail.com>
2024-04-12 01:25:49 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/640/480/any',
+    'name': '홍길동',
+    'birthday':'961222',
+    'gender':'남자',
+    'job':'대학생'
+  },
+
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/640/480/any',
+    'name': '지명헌',
+    'birthday':'000728',
+    'gender':'남자',
+    'job':'디자이너'
+  },
+
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/640/480/any',
+    'name': '추만석',
+    'birthday':'981222',
+    'gender':'남자',
+    'job':'프로그래머'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>let's go!!!</h2>
+    <div>
+      {
+        customers.map(c=>{
+          return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+          )
+        })
+      }
     </div>
   );
 }
