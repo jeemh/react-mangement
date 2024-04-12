1a2b6ada0ea271568fde4338a83167e39e941564
jeemh <jimyeongheon@gmail.com>
2024-04-12 21:54:55 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -9,35 +10,27 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+
+connection.connect();
+
 app.get('/api/customers', (req, res)=>{
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/640/480/any',
-            'name': '홍길동',
-            'birthday':'961222',
-            'gender':'남자',
-            'job':'대학생'
-        },
-        
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/640/480/any',
-            'name': '지명헌',
-            'birthday':'000728',
-            'gender':'남자',
-            'job':'디자이너'
-        },
-        
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/640/480/any',
-            'name': '추만석',
-            'birthday':'981222',
-            'gender':'남자',
-            'job':'프로그래머'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    )
 });
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
